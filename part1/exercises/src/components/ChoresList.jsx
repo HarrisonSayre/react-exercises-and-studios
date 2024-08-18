e6238c75fef4e1aa01260e638dd6936aedb1ccac
Harrison Sayre <hsayre42@gmail.com>
2024-08-18 09:25:36 -0400

    <Did chores because the exercise never said to despite it being done at the end.>

--- part1/exercises/src/components/ChoresList.jsx
+++ part1/exercises/src/components/ChoresList.jsx
@@ -1,3 +1,15 @@
+import classes from './ChoresList.module.css';
+
+
 export default function ChoresList () {
-   return <p>No content is here!</p>;
+   return( 
+      <div> 
+         <h1 className = {classes.choresHeading}>Chores</h1> 
+         <ui>
+            <li className={classes.choresText}>Exercise 1</li>
+            <li className={classes.choresText}>Demo</li>
+            <li className={classes.choresText}>Studio</li>
+         </ui>
+      </div>
+   );
 }
